05089441008dab8a9d8d4b0025892bb647bc12bf
Etch <Etch.rush@gmail.com>
2018-07-26 23:39:53 -0800

    another tweak

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,12 @@
 var mysql = require("mysql");
 
-if (process.env.JAWSDB_URL){
+if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host:'localhost',
+        host: 'localhost',
         user: "root",
-        password:"passwood",
+        password: "passwood",
         database: "burger_db"
     })
 }
