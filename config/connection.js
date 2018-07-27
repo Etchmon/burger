fdf941ab7543f94f5c1ece16df8e8edf84ca3c74
Etch <Etch.rush@gmail.com>
2018-07-26 22:29:51 -0800

    same error

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,13 @@
 var mysql = require("mysql");
 
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "passwood",
+    database: "burger_db"
+});
+
 if (process.env.JAWSDB_URL){
     connection = mysql.createConnection(process.env.JAWSBD_URL);
 } else {
@@ -9,8 +17,7 @@ if (process.env.JAWSDB_URL){
         password:"passwood",
         database: "burger_db"
     })
-};
-
+}
 connection.connect(function (err) {
     if (err) {
         console.error("error connecting: " + err.stack);
