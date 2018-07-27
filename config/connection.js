73a1263816dda29afdd4dabdd3484b7c0cecbb78
Etch <Etch.rush@gmail.com>
2018-07-26 18:17:58 -0800

    trying to deploy

--- config/connection.js
+++ config/connection.js
@@ -8,6 +8,16 @@ var connection = mysql.createConnection({
     database: "burger_db"
 });
 
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSBD_URL);
+} else {
+    connection = mysql.createConnection({
+        host:"localhost",
+        user: "root",
+        password:"passwood",
+        database: "burgers_db"
+    })
+}
 connection.connect(function (err) {
     if (err) {
         console.error("error connecting: " + err.stack);
